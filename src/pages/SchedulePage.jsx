50186a498df03fd5c767f6a37e6b091241a51bb1
anusha-anil3773 <anushaanil3773@gmail.com>
2026-01-21 18:24:54 +0530

    change time

--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -5,7 +5,7 @@ export default function SchedulePage({ changePage }) {
         <Overlay changePage={changePage} page="cover">
             <h1 className='text-xl sm:text-3xl my-2 font-["cinzel"]'>Welcoming at 10.30 am</h1>
             <img src="/assets/images/seperator.png" className="h-4" />
-            <h1 className='text-xl sm:text-3xl my-2 font-["cinzel"] leading-11'>Muhoortham between<br /> 11.05 am and 11.35 pm</h1>
+            <h1 className='text-xl sm:text-3xl my-2 font-["cinzel"] leading-11'>Muhoortham between<br /> 11.05 am and 11.35 am</h1>
             <img src="/assets/images/seperator.png" className="h-4" />
             <h1 className='text-lg sm:text-2xl my-2 font-["cinzel"] leading-11'>With best compliments from: Adithya Anil</h1>
             <img src="/assets/images/seperator.png" className="h-4" />
